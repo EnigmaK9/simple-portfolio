f6d0a86f79fccb42c8dd5fb76b0320d2f5d1dba8
EnigmaK9 <enigmak9@protonmail.com>
2023-02-05 04:23:49 -0600

    translated all, added royal purple tone reduced some descriptions as well.

---
+++ main.js
@@ -0,0 +1,10 @@
+<script>
+    function showParagraph(id) {
+        var p = document.getElementById(id);
+        if (p.style.display === "block") {
+            p.style.display = "none";
+        } else {
+            p.style.display = "block";
+        }
+    }
+</script>
