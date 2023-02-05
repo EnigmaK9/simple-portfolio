dc0c8c03322014620a7cce3d98c62b62f995cf27
EnigmaK9 <enigmak9@protonmail.com>
2023-02-05 04:36:58 -0600

    added main.js

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
-<script>
-    function showParagraph(id) {
-        var p = document.getElementById(id);
-        if (p.style.display === "block") {
-            p.style.display = "none";
-        } else {
-            p.style.display = "block";
-        }
+// hide all sections on page load
+document.addEventListener("DOMContentLoaded", function() {
+    var sections = document.querySelectorAll("main section");
+    for (var i = 0; i < sections.length; i++) {
+      sections[i].style.display = "none";
     }
-</script>
+  });
+  
+  // show the selected section and hide the others
+  function showSection(id) {
+    var sections = document.querySelectorAll("main section");
+    for (var i = 0; i < sections.length; i++) {
+      if (sections[i].id === id) {
+        sections[i].style.display = "block";
+      } else {
+        sections[i].style.display = "none";
+      }
+    }
+  }
+  
+  // add active class to selected nav link and remove from others
+  function setActiveNavLink(id) {
+    var links = document.querySelectorAll("nav a");
+    for (var i = 0; i < links.length; i++) {
+      if (links[i].id === id) {
+        links[i].classList.add("active");
+      } else {
+        links[i].classList.remove("active");
+      }
+    }
+  }
+  
+  // handle nav link clicks
+  var navLinks = document.querySelectorAll("nav a");
+  for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", function(event) {
+      event.preventDefault();
+      showSection(event.target.href.split("#")[1]);
+      setActiveNavLink(event.target.id);
+    });
+  }
+  
